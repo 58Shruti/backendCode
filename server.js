0170e2d9c8eb4648e6795baf129d49d9ce66fb3f
dev <dev@examplegmail.com>
2025-03-03 15:48:18 +0530

    server.js updated

--- server.js
+++ server.js
@@ -19,5 +19,21 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Export the Express app for Vercel
+// // Export the Express app for Vercel
+// module.exports = app;
+
+// const express = require("express");
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// // Middleware
+// app.use(express.json());
+
+// // Sample route
+// app.get("/", (req, res) => {
+//   res.send("Hello from Vercel!");
+// });
+
+// Export the Express app
 module.exports = app;
